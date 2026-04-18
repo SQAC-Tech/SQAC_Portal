bd197c20d6a3e9fdde371fce1223f0c17a77c2ef
ChiragPaul <chiragpaul6474@gmail.com>
2026-04-18 21:45:06 +0530

    Login UI and backend connected

--- Backend/lib/mailer.js
+++ Backend/lib/mailer.js
@@ -1,22 +1,22 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:processs.env.USER_EMAIL,
-        pass:process.env.USER_PASS
-    }
+  service: "gmail",
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASS,
+  },
 });
 
-const sendMail = async({to, subject, html})=>{
-    await transporter.sendMail({
-        from:`"SQAC Portal Admin" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html,
-    });
-}
+const sendMail = async ({ to, subject, html }) => {
+  await transporter.sendMail({
+    from: `"SQAC Portal Admin" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+};
 
 export default sendMail;
