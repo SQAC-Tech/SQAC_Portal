15da80ed720988f77eda9786725313e8f30152f1
MJ-1112 <m22327803@gmail.com>
2026-04-26 22:49:37 +0530

    users notice route added

--- Backend/src/models/Notice.js
+++ Backend/src/models/Notice.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const noticeSchema = new mongoose.Schema({
+    id,
     title:{type: String, required: true},
     desc:{type: String, required: true},
     Timestamp: {
