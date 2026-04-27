21a639528f54add37dc76a2f0ffdb3dd824cf684
ChiragPaul <chiragpaul6474@gmail.com>
2026-04-27 19:20:54 +0530

    mj fixed backend issues

--- Backend/src/models/Meeting.js
+++ Backend/src/models/Meeting.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const MeetingSchema = new mongoose.Schema({
-    id,
-    title:String,
-    startDate: Date,
-    startTime: new Date("1970-01-01T10:30:00Z"),
-    meetlink:String,
-    description:String
+  title: String,
+  startDate: Date,
+  startTime: {
+    type: Date,
+    default: new Date("1970-01-01T10:30:00Z"),
+  },
+  meetlink: String,
+  description: String,
 });
 
-export default mongoose.model('Meeting', MeetingSchema);
+export default mongoose.model("Meeting", MeetingSchema);
