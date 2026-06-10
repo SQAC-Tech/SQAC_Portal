b31ffbcc5ecbe73b5f361201b55787f44e6601a1
shauryaaojha <shauryaaojha@gmail.com>
2026-06-11 01:10:38 +0530

    updated cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./src/lib/db.js";
 import { requireRole } from "./src/middleware/role.middleware.js";
@@ -53,19 +55,18 @@ import certificateRoutes from "./src/routes/certificate.routes.js";
 import projectRoutes from "./src/routes/project.routes.js";
 import momRoutes from "./src/routes/mom.routes.js";
 
-dotenv.config();
-
 const app = express();
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
 // Middleware
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  }),
-);
 
 // --- Public Routes ---
 app.post("/user/create", createUser);
