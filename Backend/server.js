08d0e8c821ecb4a252e8cbf330d1a1ae0c2bd930
shauryaaojha <shauryaaojha@gmail.com>
2026-06-11 01:20:18 +0530

    Cors error

--- Backend/server.js
+++ Backend/server.js
@@ -64,7 +64,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
